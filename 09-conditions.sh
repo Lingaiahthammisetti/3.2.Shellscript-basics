c24b1bc73141db26574c71f798703905664e5f16
lingaiah-ctu <sivalingapractise555@gmail.com>
2024-07-12 12:02:23 +0530

    some messages

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -1,10 +1,20 @@
+# #!/bin/bash
+# NUMBER=$1
+
+# if [ $NUMBER -gt 10 ]
+# then
+#   echo "Given number $NUMBER is greater than 10"
+# else
+#  echo "Given number $NUMBER is less than 10"
+#  fi
+
 #!/bin/bash
+
 NUMBER=$1
 
-if [$NUMBER -gt 10]
+if [ $NUMBER -gt 10 ]
 then
-  echo "Given number $NUMBER is greater than 10"
+    echo "Given number $NUMBER is greater than 10"
 else
- echo "Given number $NUMBER is less than 10"
- fi
-
+    echo "Given number $NUMBER is less than 10" 
+fi
