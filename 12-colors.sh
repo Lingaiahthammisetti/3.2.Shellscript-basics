f66c3b0d2a2e8153ab699055112c4c9acc6866d3
lingaiah-ctu <sivalingapractise555@gmail.com>
2024-07-12 16:22:18 +0530

    some messages

--- 12-colors.sh
+++ 12-colors.sh
@@ -23,10 +23,10 @@ fi
 
 if [ $USERID -ne 0 ]
 then
-   echo "Please run this script with root access"
+   echo "Please run this script with root access."
    exit 1
 else
-   echo "You are super user."
+    echo "You are super user."
 fi 
 
 dnf install mysql -y &>>$LOGFILE
