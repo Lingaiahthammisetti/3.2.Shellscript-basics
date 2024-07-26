ac90f792344cf3a0c086e11a320ce04546b31413
lingaiah-ctu <sivalingapractise555@gmail.com>
2024-07-26 09:44:58 +0530

    shell msg

---
+++ 08-special-vars copy.sh
@@ -0,0 +1,34 @@
+
+#!/bin/bash
+echo "All variables fo $@"
+echo "All variables fo $*"
+
+echo "Total Number of parameters passed $#"+ $#
+echo "Name of Script file: $0"
+echo "Process ID of the current shell script: $$"
+sleep 60 &
+echo "Process ID of last background command: $!"
+
+ls -l 1>hello.txt
+echo "Exit Status of previous command: $?"
+lsss -l 2>error.txt
+echo "Exit Status of previous command: $?"
+
+echo "Current Working Directory: $PWD"
+echo "Home directory of current user: $HOME"
+echo "Which user is running this script: $USER"
+echo "Hostname: $HOSTNAME"
+
+
+# echo "All variables fo $@"
+# echo "Number of variables  passed: $#"
+# echo "Script Name: $0"
+# echo "Current Working Directory: $PWD"
+# echo "Home directory of current user: $HOME"
+# echo "Which user is running this script: $USER"
+# echo "Hostname: $HOSTNAME"
+# echo "Process ID of the current shell script: $$"
+# sleep 60 &
+# echo "Process ID of last background command: $!"
+
+#what is different bt $@ and $*
