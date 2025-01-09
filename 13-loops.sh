6eabe80cb1b230699c222d3fc81f46cc957288a6
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-01-09 15:39:27 +0530

    shell script

--- 13-loops.sh
+++ 13-loops.sh
@@ -2,11 +2,11 @@
 
 for i in {1..20} #it will loop from 1 to 20
 do 
-   echo "The number is: "$i
+   echo $i
 done
 
 for i in {100..110} #it will loop from 1 to 20
 do 
-   echo "The number is: "$i
+   echo $i
 done
 
