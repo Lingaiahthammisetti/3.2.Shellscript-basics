07a33cf84f1fef3b3a4a7be374dde00999a2d3d3
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-01-09 16:14:42 +0530

    shell script

--- 20.2-debug-set-e-Error.sh
+++ 20.2-debug-set-e-Error.sh
@@ -2,5 +2,5 @@
 set -e  # Exit on any error
 
 echo "Starting process..."
-dnf install ansible -y # If this fails, the script will stop here
+dnf install ansibleee -y # If this fails, the script will stop here
 echo "This line will not be reached if some_command fails"
